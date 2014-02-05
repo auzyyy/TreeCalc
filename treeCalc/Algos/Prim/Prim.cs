d3c8c0d924d1302d19a2ae5eae8b87fd8cd1f23e
ArmyHill01 <phill@student.neumont.edu>
2014-02-04 17:36:57 -0700

    Re-Adding removed stuff
    DON'T REMOVE MY CODE ASSHAM!!!

---
+++ treeCalc/Algos/Prim/Prim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace treeCalc.Algos.Prim
+{
+    class Prim
+    {
+        // start with lowest edge, add it to tree
+        // then use current edges to find the next lowest edge
+    }
+}
+
+
--- treeCalc/Form1.Designer.cs
+++ treeCalc/Form1.Designer.cs
@@ -124,6 +124,9 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Kruskal",
+            "Prim"});
             this.comboBox1.Location = new System.Drawing.Point(12, 14);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
